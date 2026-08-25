cf1b92e1fc273e60eda0d512820c2c6dc6c51bd8
kalpanaRudrakshi <rudrakshikalpana19@gmail.com>
2026-08-25 23:41:14 +0530

    Logic

---
+++ Lab_Programes/src/com/labprogrames/Logic.java
@@ -0,0 +1,13 @@
+package com.labprogrames;
+
+public class Logic {
+
+	public static void main(String[] args) {
+		
+int q=5;
+String k="kalpana";
+System.out.println(q);
+System.out.println(k);
+	}
+
+}
